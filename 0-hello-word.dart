d361f9d8a8f9770affda390b6a8447edf07df7b7
Carlos Nantes <cancastilho@gmail.com>
2020-10-03 08:43:43 -0300

    Adiciona hello world.

---
+++ 0-hello-word.dart
@@ -0,0 +1,13 @@
+void main() {
+  print('Hello World');
+}
+
+/**
+ * Conclusão:
+ * - main() é a função principal de códigos em dart.
+ * - ; obrigatório no final de comandos.
+ * - print imprime texto.
+ * - Strings podem ser representadas entre ''.
+ * - void é tipo de retorno vazio da função.
+ * - Comentários são feitos usando /* */
+ */
