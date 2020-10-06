b458d84ba3bfc822596b5b173182252f2882b7d7
Carlos Nantes <cancastilho@gmail.com>
2020-10-06 01:19:52 -0300

    Trata exceção em await.

--- 3-future.dart
+++ 3-future.dart
@@ -5,7 +5,7 @@ void main() {
   buscarTextoComAsyncAwait();
 }
 
-void buscarTextoComFuture(){
+void buscarTextoComFuture() {
   print("buscarTextoComFuture:");
   var deveFalhar = false;
   buscarTexto(deveFalhar)
@@ -18,8 +18,8 @@ void buscarTextoComFuture(){
       .then((texto) => print(texto))
       .catchError((erro) => print('Erro: $erro'))
       .whenComplete(() => print('Executa sempre.'));
-
 }
+
 Future<String> buscarTexto(bool deveFalhar) {
   Future<String> promise = Future<String>.delayed(Duration(seconds: 3), () {
     if (deveFalhar) {
@@ -30,10 +30,18 @@ Future<String> buscarTexto(bool deveFalhar) {
   return promise;
 }
 
-void buscarTextoComAsyncAwait() async{
+void buscarTextoComAsyncAwait() async {
   print('buscarTextoComAsyncAwait:');
   String texto = await buscarTexto(false);
-  print('AsyncAwait '+ texto);
+  print('AsyncAwait ' + texto);
+
+  try {
+    texto = await buscarTexto(true);
+  } catch (erro) {
+    print('Erro: $erro no async await.');
+  } finally {
+     print('Executa sempre no async await.');
+  }
 }
 
 /**
@@ -42,4 +50,5 @@ void buscarTextoComAsyncAwait() async{
  * - É possível usar interpolação de variáveis em string como em 'Erro: $erro'.
  * - Async Await é uma forma de escrever código asíncrono porém com legibilidade de código síncrono.
  * - Await deve vir antes de uma função ou método que retorna um Future.
+ * - Exceções são capturadas no await com são capturadas em código síncrono.
  */
